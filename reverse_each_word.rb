d0d9e5e5034459fcad1472904ad564f465716d3f
Tom ODay <tomoday91@gmail.com>
2020-09-08 20:36:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(input)
   input_array = input.split(" ")
-  input_array.collect{|element| element.reverse}
-  input_array.join(" ")
+  new_array = [input_array.collect{|element| element.reverse}]
+  new_array.join(" ")
 end
